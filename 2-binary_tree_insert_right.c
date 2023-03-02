d4c24f75383a3f5a83358d3a00ade5bb4fa30034
adjerry <adjerusa@gmail.com>
2023-03-02 06:17:50 -0800

    task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+#include "binary_trees.h"
 /**
 * binary_tree_insert_right - add a node in the right of the parent
 * if it exists it move down one level and add the new node first
@@ -6,16 +6,13 @@
 * @value: value of the node
 * Return: NULL if it fails or the new node
 */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
-
 if (parent == NULL)
 {
 return (NULL);
 }
-
 new_node = binary_tree_node(parent, value);
 if (new_node == NULL)
 {
