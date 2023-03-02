fcaaed22ead3efafe8ecdf3c8250c3e113626adc
adjerry <adjerusa@gmail.com>
2023-03-02 07:17:56 -0800

    task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,23 +6,23 @@
 * @value: value of the node
 * Return: NULL if it fails or the new node
 */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
-if (parent == NULL)
+binary_tree_t *new_node;
+if (parent == NULL)
 {
-return (NULL);
+return (NULL)
 }
-new_node = binary_tree_node(parent, value);
-if (new_node == NULL)
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
 {
-return (NULL);
+return (NULL);
 }
-if (parent->right != NULL)
+if (parent->right != NULL)
 {
-new_node->right = parent->right;
-parent->right->parent = new_node;
+new-node->right = parent->right;
+parent->right->parent = new_node;
 }
-parent->right = new_node;
-return (new_node);
+parent->right = new_node;
+return (new_node);
 }
