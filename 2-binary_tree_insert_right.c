aef5cdd7fefabc93d080dbdfbc3cf79071dccb88
adjerry <adjerusa@gmail.com>
2023-03-02 07:22:55 -0800

    task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *new_node;
 if (parent == NULL)
 {
-return (NULL)
+return (NULL);
 }
 new_node = binary_tree_node(parent, value);
 if (new_node == NULL)
@@ -20,7 +20,7 @@ return (NULL);
 }
 if (parent->right != NULL)
 {
-new-node->right = parent->right;
+new_node->right = parent->right;
 parent->right->parent = new_node;
 }
 parent->right = new_node;
