3c7a1f594589be7482270a9c25c881f01f35ba92
adjerry <adjerusa@gmail.com>
2023-03-03 11:42:26 -0800

    task

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,7 +7,7 @@
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 binary_tree_t *pivot;
-if (tree == NULL || tree->right == NULL);
+if (tree == NULL || tree->right == NULL)
 {
 return (NULL);
 }
