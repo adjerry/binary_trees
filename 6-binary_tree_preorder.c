0367e4f10364074d0a348d2171bddf1f93a2b70d
adjerry <adjerusa@gmail.com>
2023-03-11 17:15:25 -0800

    task

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - function that goes through a binary tree
  * @tree: tree to go through
  * @func: function to use
  * Return: nothing
